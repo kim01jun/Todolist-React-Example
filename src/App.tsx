926d0dc049bd488822ba91f20473f0eb03196297
WonJunKim <kimjnsjwj@gmail.com>
2019-05-16 22:01:22 +0900

    Delete serviceWorker

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
 }
 
 export default App;
