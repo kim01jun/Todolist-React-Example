857662d4c0bc2bdf283ebd5c80d22a8a4f8a9b73
WonJunKim <kimjnsjwj@gmail.com>
2019-05-20 13:32:23 +0900

    Update workspace

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { HeaderDiv } from './style';
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
+      <HeaderDiv>
         
-      </div>
+      </HeaderDiv>
     );
   }
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
+import Header from '../components/Header/Header';
 import Login from '../components/Login/Login';
 import Start from '../components/Start/Start';
-import Todos from '../components/Todos';
+import Todos from '../components/Todos/Todos';
 
 interface MainProps { }
 
